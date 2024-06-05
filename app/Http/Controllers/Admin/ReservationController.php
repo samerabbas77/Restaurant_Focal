3e33ff82749a0ccbd5759ef5e2103e24be904fa0
FaezaAldarweesh <faeza.aldarweesh@gmail.com>
2024-06-05 10:22:41 +0300

    faeza

---
+++ app/Http/Controllers/Admin/ReservationController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+use App\Models\User;
+use App\Models\Table;
+use App\Models\reservation;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ReservationRequest;
+use App\Http\Requests\Reservation_updateRequest;
+
+
+class ReservationController extends Controller
+{
+    public function index()
+    {
+        try{
+            $reservations = reservation::all();
+            $users = User::all();
+            $tables = Table::all();
+            return view('Admin.reservation',compact('reservations' , 'users' , 'tables'));
+        }catch (\Exception $e) {
+            return redirect()->back()->with('error', 'An error occurred  ' . $e->getMessage());
+        }    
+    }
+
+//========================================================================================================================
+
+    public function store(ReservationRequest $request)
+    {
+ 
+        try{
+           $request->validated();
+
+           $reservation = new reservation();
+           $reservation->user_id = $request->user_id;
+           $reservation->table_id = $request->table_id;
+           $reservation->start_date = $request->start_date;
+           $reservation->end_date = $request->end_date;
+           $reservation->save();
+
+           session()->flash('Add','Add Susseccfully');
+           return redirect()->route('reservation.store');
+        }catch (\Exception $e) {
+            return redirect()->back()->with('error', 'An error occurred  ' . $e->getMessage());
+        }  
+    }
+
+//========================================================================================================================
+
+    public function update(Reservation_updateRequest $request, reservation $reservation)
+    {
+
+        try{
+           $request->validated();
+
+           $reservation->user_id = $request->user_id;
+           $reservation->table_id = $request->table_id;
+           $reservation->start_date = $request->start_date;
+           $reservation->end_date = $request->end_date;
+           $reservation->save();
+
+           session()->flash('edit','ُEdit Susseccfully');
+           return redirect()->route('reservation.update');
+        }catch (\Exception $e) {
+            return redirect()->back()->with('error', 'An error occurred  ' . $e->getMessage());
+        }  
+    }
+
+//========================================================================================================================
+
+    public function destroy(reservation $reservation)
+    {
+        try{
+           $reservation->delete();
+
+           session()->flash('delete','Delete Susseccfully');
+           return redirect()->route('reservation.destroy') ;
+        }catch (\Exception $e) {
+            return redirect()->back()->with('error', 'An error occurred  ' . $e->getMessage());
+        }  
+    }
+//========================================================================================================================
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AdminController;
 
-use App\Http\Controllers\Admin\TableController;
-
-
 use App\Http\Controllers\Admin\DishController;
+
+
+use App\Http\Controllers\Admin\TableController;
+use App\Http\Controllers\Admin\ClientController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ReservationController;
 
 
 
@@ -34,9 +36,18 @@ Auth::routes(['register' => false]); //إيقاف عمل راوت تسجيل ا�
 
 Route::group(['middleware' => ['auth', 'check.username']], function () {
     Route::get('/home', [HomeController::class, 'index'])->name('home');
-    Route::resource('tables',TableController::class);
     Route::resource('/categories',CategoryController::class);
+
     Route::resource('/dishes',DishController::class);
+
+    Route::resource('/tables',TableController::class);
+
+    Route::resource('reservation',ReservationController::class);
+    Route::resource('client',ClientController::class);
+
+    // Route::resource('order',OrderController::class);
+    
+    // Route::resource('reviews',ReviewController::class);
 
 });
 
