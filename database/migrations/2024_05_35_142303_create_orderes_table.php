5e5953bf2855cd419c8ccd0d18a1899afb4507fa
samerabbas77 <samer23abbas@gmail.com>
2024-06-05 16:59:36 +0300

    delet some js

--- database/migrations/2024_05_35_142303_create_orderes_table.php
+++ database/migrations/2024_05_35_142303_create_orderes_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('orderies', function (Blueprint $table) {
+        Schema::create('orderes', function (Blueprint $table) {
             $table->id();
             $table->string("status");
             $table->foreignId("user_id")->constrained('users')->onDelete('cascade');
