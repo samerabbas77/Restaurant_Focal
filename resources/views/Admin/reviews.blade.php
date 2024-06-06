7df676ad3154889965735433965f6b3939902578
samerabbas77 <samer23abbas@gmail.com>
2024-06-06 19:56:01 +0300

    Add_Reviewes_table

--- resources/views/Admin/reviews.blade.php
+++ resources/views/Admin/reviews.blade.php
@@ -33,6 +33,15 @@
         </ul>
     </div>
 @endif
+ <!-- Display session errors -->
+ @if (session('error'))
+ <div class="alert alert-danger alert-dismissible fade show" role="alert">
+	 <strong>{{ session('error') }}</strong>
+	 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+		 <span aria-hidden="true">&times;</span>
+	 </button>
+ </div>
+@endif
 
 @if (session()->has('delete'))
     <div class="alert alert-danger alert-dismissible fade show" role="alert">
