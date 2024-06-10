1ddebf345c30d1f88b467600df438ceb4a46c083
SafaaNahhas <Safaa.Hu.Nahhas@gmail.com>
2024-06-10 23:29:27 +0300

    edit api reservation

--- app/Http/Controllers/ApiController/TableController.php
+++ app/Http/Controllers/ApiController/TableController.php
@@ -4,35 +4,37 @@ namespace App\Http\Controllers\ApiController;
 
 use App\Models\Table;
 use Illuminate\Http\Request;
+use App\Http\trait\ApiResponse;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\TableResource;
 
 class TableController extends Controller
 {
+    use ApiResponse;
     public function index()
     {   try {
         $tables = Table::whereNull('deleted_at')->get();
-        return TableResource::collection($tables);
+        return $this->successResponse(TableResource::collection($tables), 'Tables show successfully.');
     } catch (\Exception $e) {
-        return response()->json(['error' => 'An error occurred while fetching the tables.'], 500);
+        return $this->errorResponse('An error occurred while fetching the tables.', ['error' => $e->getMessage()]);
     }
     }
    // ==================================================================================================================
     public function available()
     {    try {
         $tables = Table::where('Is_available', 'available')->whereNull('deleted_at')->get();
-        return TableResource::collection($tables);
+        return $this->successResponse(TableResource::collection($tables), 'Available tables show successfully.');
     } catch (\Exception $e) {
-        return response()->json(['error' => 'An error occurred while fetching the available tables.'], 500);
+        return $this->errorResponse('An error occurred while fetching the available tables.', ['error' => $e->getMessage()]);
     }
     }
     // ==================================================================================================================
     public function filterByChairs($number)
     {     try {
         $tables = Table::where('chair_number', $number)->whereNull('deleted_at')->get();
-        return TableResource::collection($tables);
+        return $this->successResponse(TableResource::collection($tables), 'Tables filtered by chair number show successfully.');
     } catch (\Exception $e) {
-        return response()->json(['error' => 'An error occurred while filtering tables by chair number.'], 500);
+        return $this->errorResponse('An error occurred while filtering tables by chair number.', ['error' => $e->getMessage()]);
     }
     }
     // ==================================================================================================================
@@ -42,9 +44,9 @@ class TableController extends Controller
                     ->where('chair_number', $number)
                     ->whereNull('deleted_at')
                     ->get();
-        return TableResource::collection($tables);
+        return $this->successResponse(TableResource::collection($tables), 'Available tables filtered by chair number show successfully.');
     } catch (\Exception $e) {
-        return response()->json(['error' => 'An error occurred while filtering available tables by chair number.'], 500);
+        return $this->errorResponse('An error occurred while filtering available tables by chair number.', ['error' => $e->getMessage()]);
     }
     }
 
